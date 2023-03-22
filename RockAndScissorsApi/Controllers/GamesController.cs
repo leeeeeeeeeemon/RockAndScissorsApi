4c20f8c8ad2052f3f15d0da2dcb8f007ca83a846
leeeeeeeeeemon <emilbarish@gmail.com>
2023-03-22 17:59:16 +0300

    release

--- RockAndScissorsApi/Controllers/GamesController.cs
+++ RockAndScissorsApi/Controllers/GamesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RockAndScissorsApi.Data;
+using System.Text.Json;
 
 namespace RockAndScissorsApi.Controllers
 {
@@ -13,7 +14,7 @@ namespace RockAndScissorsApi.Controllers
             _context = context;
         }
         [HttpPost("createGame/{PlayerName}")]
-        public int CreateGame(string PlayerName)
+        public string CreateGame(string PlayerName)
         {
             var game = new Game
             {
@@ -25,7 +26,7 @@ namespace RockAndScissorsApi.Controllers
             };
             _context.Games.Add(game);
             _context.SaveChanges();
-            return game.Id;
+            return JsonSerializer.Serialize(game);
         }
 
         [HttpPost("{gameId}/join/{PlayerName}")]
@@ -41,7 +42,7 @@ namespace RockAndScissorsApi.Controllers
                     {
                         game.Player2Name = PlayerName;
                         game.BoardState = "- - - - - - - - -";
-                        answer = "You are successfully connected";
+                        answer = JsonSerializer.Serialize(game);
                         _context.SaveChanges();
                     } else
                     {
@@ -70,7 +71,7 @@ namespace RockAndScissorsApi.Controllers
                 answer = "Game not found";
             } else
             {
-                answer = $"Player1Name: {game.Player1Name}, Player2Name: {game.Player2Name}, CurrentPlayerTurn: {game.CurrentPlayerName}, BoardState: {game.BoardState}, IsGameOver: {game.IsGameOver}";
+                answer = JsonSerializer.Serialize(game);
             }
             return answer;
             
@@ -83,37 +84,71 @@ namespace RockAndScissorsApi.Controllers
             Game game = _context.Games.FirstOrDefault(g => g.Id == gameId);
             if (game != null)
             {
-                if(game.CurrentPlayerName == PlayerName)
+                if (game.IsGameOver == false)
                 {
-                    string[] gameBoard = game.BoardState.Split(new char[] { ' ' });
-                    if (row > 0 && column > 0 && row <=3 && column <= 3)
+                    if (game.CurrentPlayerName == PlayerName)
                     {
-                        int pos = (row - 1) * 3 + column;
-                        if (gameBoard[pos] != "-")
+                        if (game.Player2Name != "No one")
                         {
-                            if (game.CurrentPlayerName == game.Player1Name) gameBoard[pos] = "X";
-                            else gameBoard[pos] = "O";
-                            string resultBoard = "";
-                            foreach (string board in gameBoard)
+                            string[] gameBoard = game.BoardState.Split(new char[] { ' ' });
+                            if (row > 0 && column > 0 && row <= 3 && column <= 3)
                             {
-                                resultBoard += board;
-                                resultBoard += " ";
-                            }
-                            game.BoardState = resultBoard;
-                            game.CurrentPlayerName = game.Player2Name;
-                            _context.SaveChanges();
-                            answer = $"Success! BoardState: {game.BoardState}";
-                        }
-                        answer = "The field is taken";
-                    }
-                    else
-                    {
-                        answer = "Column or line is not properly entered";
-                    }
+                                int pos = (row - 1) * 3 + column - 1;
+                                if (gameBoard[pos] == "-")
+                                {
 
+                                    if (game.CurrentPlayerName == game.Player1Name) gameBoard[pos] = "X";
+                                    else gameBoard[pos] = "O";
+                                    string resultBoard = "";
+                                    foreach (string board in gameBoard)
+                                    {
+                                        resultBoard += board;
+                                        resultBoard += " ";
+                                    }
+
+                                    if (CheckWinner(gameBoard))
+                                    {
+                                        game.IsGameOver = true;
+                                        game.WinnerName = game.CurrentPlayerName;
+                                    }
+
+                                    game.BoardState = resultBoard;
+
+                                    if (game.CurrentPlayerName == game.Player1Name)
+                                        game.CurrentPlayerName = game.Player2Name;
+                                    else game.CurrentPlayerName = game.Player1Name;
+
+                                    _context.SaveChanges();
+                                    answer = JsonSerializer.Serialize(game);
+                                    if (game.IsGameOver == true && game.WinnerName != "No one")
+                                    {
+                                        answer += $"The winner is {game.WinnerName}";
+                                    }
+                                    else if (game.IsGameOver == true)
+                                    {
+                                        answer += "Game over, draw.";
+                                    }
+                                } else
+                                {
+                                    answer = "The field is taken";
+                                }
+                            }
+                            else
+                            {
+                                answer = "Column or line is not properly entered";
+                            }
+                        }
+                        else
+                        {
+                            answer = "Wainting for second player";
+                        }
+                    } else
+                    {
+                        answer = "Its not your turn or You are not a member of the game";
+                    }
                 } else
                 {
-                    answer = "Its not your turn";
+                    answer = "Game already end";
                 }
             } else
             {
@@ -122,11 +157,43 @@ namespace RockAndScissorsApi.Controllers
             return answer;
         }
 
-        //[HttpGet("{gameId}/winner")]
-        //public IActionResult CheckWinner(Guid gameId)
-        //{
-        //    var winner = _gameService.CheckWinnerAsync(gameId);
-        //    return Ok(winner);
-        //}
+        private bool CheckWinner(string[] boardState)
+        {
+            bool IsWin = false;
+            if ((boardState[0] == "X" && boardState[1] == "X" && boardState[2] == "X") ||
+                ((boardState[3] == "X" && boardState[4] == "X" && boardState[5] == "X")) ||
+                (boardState[6] == "X" && boardState[7] == "X" && boardState[8] == "X"))
+                IsWin = true;
+            if ((boardState[0] == "X" && boardState[3] == "X" && boardState[6] == "X") ||
+                (boardState[1] == "X" && boardState[4] == "X" && boardState[7] == "X") ||
+                (boardState[2] == "X" && boardState[5] == "X" && boardState[8] == "X"))
+                IsWin = true;
+            if ((boardState[0] == "X" && boardState[4] == "X" && boardState[8] == "X") ||
+                (boardState[2] == "X" && boardState[4] == "X" && boardState[6] == "X"))
+                IsWin = true;
+            if ((boardState[0] == "O" && boardState[1] == "O" && boardState[2] == "O") ||
+                ((boardState[3] == "O" && boardState[4] == "O" && boardState[5] == "O")) ||
+                (boardState[6] == "O" && boardState[7] == "O" && boardState[8] == "O"))
+                IsWin = true;
+            if ((boardState[0] == "O" && boardState[3] == "O" && boardState[6] == "O") ||
+                (boardState[1] == "O" && boardState[4] == "O" && boardState[7] == "O") ||
+                (boardState[2] == "O" && boardState[5] == "O" && boardState[8] == "O"))
+                IsWin = true;
+            if ((boardState[0] == "O" && boardState[4] == "O" && boardState[8] == "O") ||
+                (boardState[2] == "O" && boardState[4] == "O" && boardState[6] == "O"))
+                IsWin = true;
+            return IsWin;
+        }
+
+        [HttpGet("{gameId}/winner")]
+        public string CheckWinner(int gameId)
+        {
+            string answer = "";
+            var game = _context.Games.FirstOrDefault(g => g.Id == gameId);
+            if (game == null) answer = "A game with this id does not exist";
+            else if (game.IsGameOver == false) answer = "The game isn't over yet.";
+            else answer = JsonSerializer.Serialize(game);
+            return answer;
+        }
     }
 }
